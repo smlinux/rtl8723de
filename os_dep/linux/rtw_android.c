834d40962888c86ce80cd794bffe9987b7b51904
Gavin Dsouza <gavin18d@gmail.com>
2019-03-08 00:25:46 +0530

    fix build for Linux-5.0
    Signed-off-by: smlinux <smartlinux@ya.ru>

--- os_dep/linux/rtw_android.c
+++ os_dep/linux/rtw_android.c
@@ -626,7 +626,13 @@ int rtw_android_priv_cmd(struct net_device *net, struct ifreq *ifr, int cmd)
 		goto exit;
 	}
 
-	if (!access_ok(VERIFY_READ, priv_cmd.buf, priv_cmd.total_len)) {
+	/* 
+	* access_ok ==> function declared in arch/csky/include/asm/uaccess.h
+	* till 4.20.14 ==> static inline int access_ok(int type, const void *addr, unsigned long size)
+	* post 5.0.0 ==> static inline int access_ok(const void *addr, unsigned long size)
+	* type int was a previously unused variable 
+	*/
+	if (!access_ok(priv_cmd.buf, priv_cmd.total_len)) {
 		RTW_INFO("%s: failed to access memory\n", __FUNCTION__);
 		ret = -EFAULT;
 		goto exit;
